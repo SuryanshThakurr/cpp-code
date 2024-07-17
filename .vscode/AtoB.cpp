9f9db99c647c7b2670542125e7dd7626a658bff0
Suryansh Singh <suryansht565@gmail.com>
2024-07-18 00:24:56 +0530

    practice

---
+++ .vscode/AtoB.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+char a;
+a='A';
+a++;
+cout<< "Result:"<<a;
+
+
+}
---
+++ .vscode/sizeofrealconstant.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a;
+cout<< "Enter the no.:";
+cin>> a;
+cout<<"Size:"<< sizeof(a);
+
+}
