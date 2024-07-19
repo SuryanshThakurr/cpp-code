71a1226eba78f7177c0d6eb25801aeb3bfe0ffac
Suryansh Singh <suryansht565@gmail.com>
2024-07-19 23:51:21 +0530

    practice

---
+++ .vscode/sizeofcharflodoub.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+char  a;
+float b;
+double c;
+int d;
+cin>> a>>b>>c>>d;
+cout<<"Size:"<<endl<< "char "<<sizeof(a)<<endl<<"float "<<sizeof(b)<<endl<<"double "<<sizeof(c)<<endl<<"int "<<sizeof(d);
+} 
