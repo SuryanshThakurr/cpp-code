3d125844ce862f6e60f7a843f66f60956e9aa575
Suryansh Singh <suryansht565@gmail.com>
2024-07-21 16:45:59 +0530

    practice

---
+++ .vscode/divisiblebyfive.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the value:";
+    cin>>a;
+    if(a%5==0)
+    {
+        cout<<"The given value is dividible by five:";
+
+    }
+    else
+    {
+        cout<<"The given value is not divisible by five:";
+    }
+}
