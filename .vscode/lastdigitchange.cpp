dbe1fe1621677af3139255e83ae5033dfa0f8845
Suryansh Singh <suryansht565@gmail.com>
2024-07-21 15:58:41 +0530

    practice

---
+++ .vscode/lastdigitchange.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cout<<"Enter the value:";
+  cin>>a;
+  cout<<"Enter the digt:";
+  cin>>b;
+  c=(a)*10+b;
+  cout<<"result"<<c;
+
+}
+
