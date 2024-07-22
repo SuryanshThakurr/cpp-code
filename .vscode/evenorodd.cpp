bab5497a723bb641194d2d6d66f920fcbb5ccba2
Suryansh Singh <suryansht565@gmail.com>
2024-07-22 13:05:03 +0530

    practice

---
+++ .vscode/evenorodd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the value:";
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<"The given value is even:";
+
+    }
+    else
+    {
+        cout<<"The given value is odd:";
+    }
+}
