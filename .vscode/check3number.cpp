06ed382cab4b81bbf182bbfff3b371cc95dbb195
Suryansh Singh <suryansht565@gmail.com>
2024-07-22 13:54:13 +0530

    practice

---
+++ .vscode/check3number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the value:";
+    cin>>a;
+    if(a>99&&a<=999)
+    {
+        cout<<"The given value is three digit no.:";
+
+    }
+    else
+    {
+        cout<<"The given value is not three digit no.:";
+    }
+}
