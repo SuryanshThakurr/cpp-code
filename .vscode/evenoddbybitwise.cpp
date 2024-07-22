ac1f99b4e1fa072bf8a45b1755fd7e35aca5bcf6
Suryansh Singh <suryansht565@gmail.com>
2024-07-22 13:26:49 +0530

    practice

---
+++ .vscode/evenoddbybitwise.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the value:";
+    cin>>a;
+    if(a&1)
+    {
+        cout<<"The given value is odd:";
+
+    }
+    else
+    {
+        cout<<"The given value is even:";
+    }
+}
